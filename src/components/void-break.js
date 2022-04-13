0a07aa197f2bf6a4befa11a1e3548b4816477ba0
Noah Emke <noahremke@gmail.com>
2022-04-13 05:57:53 -0400

    Improved auto canvas sizing and added loading state

--- src/components/void-break.js
+++ src/components/void-break.js
@@ -5,6 +5,9 @@ canvas {
 	height: 100%;
 	width: 100%;
 }
+:host {
+	overflow: hidden;
+}
 </style>
 <canvas>
 `;
@@ -19,7 +22,70 @@ class VoidBreak extends HTMLElement {
 
 		this.canvas = this.shadowRoot.querySelector('canvas');
 		this.ctx = this.canvas.getContext('2d');
-		this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
+
+		this.resolution = 1080; //TODO: Get this from localstorage or defaults json
+		document.defaultView.onresize = () => {
+			this.canvas.width = (this.offsetWidth / this.offsetHeight) * this.resolution;
+			this.canvas.height = this.resolution;
+		};
+
+		document.defaultView.onresize();
+		this.state = 'loading';
+		this.loading = 0;
+		this.lastFrameTime = window.performance.now();
+		this.loop();
+	}
+
+	// Draw a loading screen
+	drawLoading(percent) {
+		this.ctx.save();
+
+		// Clear
+		this.ctx.fillStyle = 'black';
+		this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.fill();
+
+		this.ctx.fillStyle = 'white';
+		this.ctx.strokeStyle = 'white';
+
+		// Draw text
+		this.ctx.textAlign = 'center';
+		this.ctx.font = '30px Comic Sans MS';
+		this.ctx.fillText("Loading...", this.canvas.width / 2, this.canvas.height / 2 - 20);
+
+		// Draw outline
+		this.ctx.beginPath();
+		this.ctx.rect(this.canvas.width / 3, this.canvas.height / 2, this.canvas.width / 3, 30);
+		this.ctx.stroke();
+
+		// Draw fill
+		this.ctx.beginPath();
+		this.ctx.rect(
+			this.canvas.width / 3, 
+			this.canvas.height / 2, 
+			this.canvas.width / 3 * (percent / 100), 
+			30);
+		this.ctx.fill();
+
+		this.ctx.restore();
+	}
+
+	loop() {
+		requestAnimationFrame(()=>this.loop());
+
+		// Keep time
+		const frameTime = window.performance.now();
+		const deltaT = frameTime - this.lastFrameTime;
+		this.lastFrameTime = frameTime;
+
+		switch (this.state) {
+			case 'loading':
+				this.drawLoading(this.loading);
+				break;
+		
+			default:
+				break;
+		}
 	}
 }
 
