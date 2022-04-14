9f9f52368bcaa282c1a7b40473050fed0cca3ebc
Noah Emke <noahremke@gmail.com>
2022-04-13 21:23:35 -0400

    Added title screen with title

--- src/components/void-break.js
+++ src/components/void-break.js
@@ -1,3 +1,4 @@
+//#region Canvas element
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
@@ -11,6 +12,7 @@ canvas {
 </style>
 <canvas>
 `;
+//#endregion
 
 class VoidBreak extends HTMLElement {
 	constructor() {
@@ -23,12 +25,14 @@ class VoidBreak extends HTMLElement {
 		this.canvas = this.shadowRoot.querySelector('canvas');
 		this.ctx = this.canvas.getContext('2d');
 
+		// Resolution and autosizing
 		this.resolution = 1080; //TODO: Get this from localstorage or defaults json
 		document.defaultView.onresize = () => {
 			this.canvas.width = (this.offsetWidth / this.offsetHeight) * this.resolution;
 			this.canvas.height = this.resolution;
 		};
 
+		// init
 		document.defaultView.onresize();
 		this.state = 'loading';
 		this.loading = 0;
@@ -61,9 +65,9 @@ class VoidBreak extends HTMLElement {
 		// Draw fill
 		this.ctx.beginPath();
 		this.ctx.rect(
-			this.canvas.width / 3, 
-			this.canvas.height / 2, 
-			this.canvas.width / 3 * (percent / 100), 
+			this.canvas.width / 3,
+			this.canvas.height / 2,
+			this.canvas.width / 3 * (percent / 100),
 			30);
 		this.ctx.fill();
 
@@ -71,7 +75,7 @@ class VoidBreak extends HTMLElement {
 	}
 
 	loop() {
-		requestAnimationFrame(()=>this.loop());
+		requestAnimationFrame(() => this.loop());
 
 		// Keep time
 		const frameTime = window.performance.now();
@@ -82,7 +86,37 @@ class VoidBreak extends HTMLElement {
 			case 'loading':
 				this.drawLoading(this.loading);
 				break;
-		
+
+			case 'main menu':
+				// Draw main menu
+				this.ctx.save();
+
+				// Clear
+				this.ctx.fillStyle = 'black';
+				this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
+				this.ctx.fill();
+
+				// Draw background
+				this.ctx.save();
+				let scale = this.resolution / 1024;
+				if (this.canvas.width > this.canvas.height) scale = this.canvas.width / 1024;
+				this.ctx.scale(scale, scale);
+				this.ctx.drawImage(this.images.purple6, 0, 0);
+				this.ctx.restore();
+
+				// Draw logo text TODO: should make a proper logo
+				this.ctx.fillStyle = 'red';
+				this.ctx.strokeStyle = 'white';
+				this.ctx.textAlign = 'center';
+				this.ctx.font = '300px Futura';
+				this.ctx.fillText("VOID", this.canvas.width / 2, this.canvas.height / 2 - 300);
+				this.ctx.strokeText("VOID", this.canvas.width / 2, this.canvas.height / 2 - 300);
+				this.ctx.fillText("BREAK", this.canvas.width / 2, this.canvas.height / 2 - 80);
+				this.ctx.strokeText("BREAK", this.canvas.width / 2, this.canvas.height / 2 - 80);
+
+				this.ctx.restore();
+				break;
+
 			default:
 				break;
 		}
--- src/loader.js
+++ src/loader.js
@@ -2,10 +2,29 @@ import "./components/nav-header.js";
 import "./components/void-break.js";
 
 const game = document.querySelector('void-break');
+const loadingTotal = 5.0;
 
 const imageSources = {
-	purple5: 'media/images/backgrounds/large/purple/purple-nebula-5.jpg'
+	purple5: 'media/images/backgrounds/large/purple/purple-nebula-5.jpg',
+	purple6: 'media/images/backgrounds/large/purple/purple-nebula-6.png'
 };
+
+// Load sounds
+const sounds = {};
+
+sounds.shoot1 = new Howl({ src: ['media/sounds/shoot1.wav'] });
+sounds.shoot1.volume(0.3);
+game.loading += 100.0 / loadingTotal;
+
+sounds.hit1 = new Howl({ src: ['media/sounds/hit1.wav'] });
+sounds.hit1.volume(0.5);
+game.loading += 100.0 / loadingTotal;
+
+sounds.hit2 = new Howl({ src: ['media/sounds/hit2.wav'] });
+sounds.hit2.volume(0.5);
+game.loading += 100.0 / loadingTotal;
+
+game.sounds = sounds;
 
 // Load images
 for (let imageName in imageSources) {
@@ -13,29 +32,13 @@ for (let imageName in imageSources) {
 	img.src = imageSources[imageName];
 	imageSources[imageName] = img;
 	img.onload = function () {
-		game.loading += 25;
+		game.loading += 100.0 / loadingTotal;
+		if (game.loading == 100) {
+			game.images = imageSources;
+			game.state = 'main menu';
+		}
 	}
 	img.onerror = function () {
 		console.log("ERROR: image named '" + imageName + "' at " + this.src + " did not load!");
 	}
 }
-game.images = imageSources;
-
-// Load sounds
-const sounds = {};
-
-sounds.shoot1 = new Howl({ src: ['media/sounds/shoot1.wav'] });
-sounds.shoot1.volume(0.3);
-game.loading += 25;
-
-sounds.hit1 = new Howl({ src: ['media/sounds/hit1.wav'] });
-sounds.hit1.volume(0.5);
-game.loading += 25;
-
-sounds.hit2 = new Howl({ src: ['media/sounds/hit2.wav'] });
-sounds.hit2.volume(0.5);
-game.loading += 25;
-
-game.sounds = sounds;
-
-// game.state = 'main menu';
